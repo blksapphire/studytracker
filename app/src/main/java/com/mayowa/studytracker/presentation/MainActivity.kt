8988ae8c62833dcb1ffe1eae08608d5a85e859e6
blksapphire <mayowaa416@gmail.com>
2026-09-15 15:06:52 +0100

    Fix missing Material3 imports

--- app/src/main/java/com/mayowa/studytracker/presentation/MainActivity.kt
+++ app/src/main/java/com/mayowa/studytracker/presentation/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.mayowa.studytracker.presentation
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
