ff5788807b5255726a5957f91e7faefc73b5117c
blksapphire <mayowaa416@gmail.com>
2026-09-15 15:12:47 +0100

    Fix missing Material3 Icon import

--- app/src/main/java/com/mayowa/studytracker/presentation/MainActivity.kt
+++ app/src/main/java/com/mayowa/studytracker/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mayowa.studytracker.presentation
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
