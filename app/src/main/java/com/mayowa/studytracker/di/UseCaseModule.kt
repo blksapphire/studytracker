02dc1e54fec0b3561b7ad84f147bf9a3f4cac12a
The Sapphire <114116628+blksapphire@users.noreply.github.com>
2026-09-15 22:51:49 +0100

    feat: wire adaptive study plan use case

--- app/src/main/java/com/mayowa/studytracker/di/UseCaseModule.kt
+++ app/src/main/java/com/mayowa/studytracker/di/UseCaseModule.kt
@@ -1,5 +1,6 @@
 package com.mayowa.studytracker.di
 
+import com.mayowa.studytracker.domain.usecase.BuildStudyPlanUseCase
 import com.mayowa.studytracker.domain.usecase.CalculateAdaptiveGoalUseCase
 import com.mayowa.studytracker.domain.usecase.CalculateStreakUseCase
 import dagger.Module
@@ -10,9 +11,7 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
-    @Provides
-    fun provideCalculateStreakUseCase(): CalculateStreakUseCase = CalculateStreakUseCase()
-
-    @Provides
-    fun provideCalculateAdaptiveGoalUseCase(): CalculateAdaptiveGoalUseCase = CalculateAdaptiveGoalUseCase()
+    @Provides fun provideCalculateStreakUseCase(): CalculateStreakUseCase = CalculateStreakUseCase()
+    @Provides fun provideCalculateAdaptiveGoalUseCase(): CalculateAdaptiveGoalUseCase = CalculateAdaptiveGoalUseCase()
+    @Provides fun provideBuildStudyPlanUseCase(): BuildStudyPlanUseCase = BuildStudyPlanUseCase()
 }
